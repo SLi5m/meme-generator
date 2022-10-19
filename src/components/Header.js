d7540b400d8566bda1d1c1b9678b2a17b7ac452a
SLi5m <Rogersbag@gmail.com>
2022-10-19 10:58:53 +0300

    inital commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function Header (){
+    return(
+        <header className="header">
+            <img src="./images/troll-face.png" 
+                alt="This is a troll img"
+                className="header--image" 
+            />
+            <h2 className="header--title">Header component</h2>
+            <h4 className="header--project">Astro-dynamics </h4>
+        </header>
+    )
+}
